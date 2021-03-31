bf256e201ffe9e38a25a99ae032ac57257fe7459
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-03-31 19:05:26 +0300

    Set flow view relative geometry

--- src/flow/flowview.py
+++ src/flow/flowview.py
@@ -14,21 +14,35 @@ class FlowView(LabelFrame):
     self['relief'] = RIDGE 
     self.parent['bg'] = 'bisque'
 
-    self.parent.update()
-    h = self.parent.winfo_reqheight()
-    w = self.parent.winfo_width()
+    # self.parent.update()
+    # h = self.parent.winfo_reqheight()
+    # w = self.parent.winfo_reqwidth()
 
     self.grid()
     self.rowconfigure(0, weight=1)
     self.columnconfigure(0, weight=1)
 
-    self.modules_frame = LabelFrame(self, bg="yellow", text='Modules', height=200, width=w)
-    # self.modules_frame.grid_propagate(0)
 
     self.actions_frame = FlowActions(self)   
 
+    # self.modules_frame = LabelFrame(self, bg="yellow", text='Modules', height=h-200, width=w)
+    self.modules_frame = LabelFrame(self, bg="yellow", text='Modules')
+    self.fit_modules_frame_size()
+
     self.modules_frame.grid(row=0, column=0, padx=PADX, pady=PADY)
     self.actions_frame.grid(row=4, column=0, padx=PADX, pady=PADY)
+
+
+  def fit_modules_frame_size(self):
+    self.parent.update()
+    h = self.parent.winfo_reqheight()
+    w = self.parent.winfo_reqwidth()
+    self.actions_frame.update()
+    self.modules_frame['height'] = h - self.actions_frame.winfo_reqheight() - PADY*4
+    self.modules_frame['width'] = w
+    print("FlowView", w, h, self.actions_frame.winfo_reqheight())
+    # do not resize the flow frame after a widget will be added
+    self.modules_frame.grid_propagate(0)
 
 
   def show_modules(self, modules):
--- src/mainwindow.py
+++ src/mainwindow.py
@@ -7,39 +7,34 @@ from .flow import FlowController
 class MainWindow():
   def __init__(self, root):
     self.root = root
-    # self.PADX = 10
-    # self.PADY = 10
    
-    self.root.update()
-    h = self.root.winfo_reqheight()
-    w = self.root.winfo_width()
-
     self.main_frame = Frame(self.root)
     self.main_frame.grid()
 
     # create all of the main containers
     self.actions_frame = MainActions(self.main_frame)
 
-    # flow_height = calculate_reminder_height(self.root, [self.actions_frame])
-
     self.flow_frame = Frame(self.main_frame)
+    self.fit_flow_frame_size()
     self.flow_controller = FlowController(self.flow_frame)
-
-    # layout all of the main container
-    # self.main_frame.rowconfigure(0, weight=1)
-    # self.main_frame.columnconfigure(0, weight=1)
 
     self.flow_frame.grid(row=0, column=0, padx=PADX, pady=PADY, sticky=E+W)
     self.actions_frame.grid(row=1, column=0, padx=PADX, pady=PADY, sticky=E+W)
 
-    # do not resize the flow frame after a widget will be added
+    # Bind to actions panel
+    self.actions_frame.btn_exit.bind("<Button>", self.exit)
+
+
+  def fit_flow_frame_size(self):
+    self.root.update()
+    h = self.root.winfo_reqheight()
+    w = self.root.winfo_width()
     flow_height = calculate_reminder_height(self.root, [self.actions_frame])
     self.flow_frame['height'] = flow_height
     self.flow_frame['width'] = w
+    
+    # do not resize the flow frame after a widget will be added
     self.flow_frame.grid_propagate(0)
-
-    # Bind to actions panel
-    self.actions_frame.btn_exit.bind("<Button>", self.exit)
 
   def exit(self, event):
     exit(0)
