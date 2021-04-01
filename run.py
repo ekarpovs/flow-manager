f0506acbcbdbc0a2441106619ff75865f46938e8
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-04-01 10:25:27 +0300

    Change main window geometry

--- run.py
+++ run.py
@@ -6,7 +6,10 @@ from src import MainWindow
 if __name__ == '__main__':
   root = Tk()
   root.title("Flow Manager")
-  root.state('zoomed')
+  # root.state('zoomed')
+  sw = root.winfo_screenwidth ()
+  sh = root.winfo_screenheight()
+  root.geometry("{}x{}+{}+{}".format(sw-10, sh-70, 0,0))
   root.resizable(0, 0) 
 
   myapp = MainWindow(root)
