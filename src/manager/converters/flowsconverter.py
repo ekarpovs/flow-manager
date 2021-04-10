c5f3b4744fa05e8626bd31b60c1ee9a71ca89a66
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-04-10 09:17:10 +0300

    Implement flow converter

--- src/manager/converters/flowsconverter.py
+++ src/manager/converters/flowsconverter.py
@@ -1,3 +1,4 @@
+from tkinter.constants import S
 from .converter import Converter
 
 class FlowsConverter(Converter):
@@ -6,3 +7,16 @@ class FlowsConverter(Converter):
     self.parent = parent 
 
     print("FLOWS-CONVERTER")
+
+
+  @staticmethod
+  def split_ws_item(item):
+    name, path = item.split('<')
+    name = name[:-1]
+    path =  path[:-1]
+
+    return path, name
+    
+  @staticmethod
+  def get_empty_flow():
+    return {'steps':[]}
