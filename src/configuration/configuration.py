9f3a0686a5a6c3b0f851ecf18d283112ce37ab6f
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-04-22 10:19:39 +0300

    Simplify configuration

--- src/configuration/configuration.py
+++ src/configuration/configuration.py
@@ -8,47 +8,32 @@ class Configuration():
 
   # Loaders
   def load_configuration(self):
-    # TODO read from config.json
+    # Read from config.json
     ffn = "{}.json".format('config')
     with open(ffn, 'rt') as ws:
-      cfg = json.load(ws)
-
-
-    self.factory_path = cfg['factory'] 
-    self.input_paths = cfg['images']
-    self.result_path = cfg['results']
-    self.modules_paths = cfg['modules']
-    self.worksheets_paths = cfg['worksheets']
+      self.cfg = json.load(ws)
 
     # Set paths for access factory and modules outside of the application
-    for path in self.modules_paths:
+    for path in self.cfg['modules']:
       sys.path.append(path)
+    
     # Till the operation-loader is not installed via pip
-    sys.path.append(self.factory_path)
+    sys.path.append(self.cfg['factory'])
     import factory
     self.factory = factory
   
-
-  # def load_config_file(self):
-  #   ffn = "{}.json".format('config')
-  #   with open(ffn, 'rt') as ws:
-  #     cfg = json.load(ws)
-  #     print(cfg)
-
-  #   return cfg
-
 
   def get_factory(self):
     return self.factory
 
   def get_input_paths(self):
-    return self.input_paths
+    return self.cfg['images']
 
   def get_result_path(self):
-    return self.result_path
+    return self.cfg['results']
 
   def get_modules_paths(self):
-    return self.modules_paths
+    return self.cfg['modules']
 
   def get_worksheets_paths(self):
-    return self.worksheets_paths
+    return self.cfg['worksheets']
