7c2c42ab9070938aa5b0847ce1dcecae5d7494e8
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-04-28 14:32:38 +0300

    Fix state issues

--- src/manager/runner/runner.py
+++ src/manager/runner/runner.py
@@ -1,15 +1,15 @@
 from tkinter.constants import E
+from .state import State
 from .context import Context
 from .contextstack import ContextStack
 from .wrapper import flowoperation
 
 class Runner():
   def __init__(self, cfg):
-
+    self.state = State()
     self.contextstack = ContextStack()
     self.get = cfg.get_factory().get
     self.cv2image = None
-    self.counter = 0
  
   def set_input_image(self, cv2image):
     self.cv2image = cv2image
@@ -29,9 +29,10 @@ class Runner():
 
     return operation
 
+
   def init_step(self):
     if self.contextstack.isEmpty():
-      self.counter = 0
+      # self.state.reset()
       kwargs = {}
       kwargs['orig'] = self.cv2image
       kwargs['image'] = self.cv2image
@@ -40,33 +41,30 @@ class Runner():
 
     return kwargs
 
-  def increment_counter(self):
-    self.counter += 1
-    return
-
-  def dencrement_counter(self):
-    self.counter -= 1
-    return
-
 
   def run(self, flow_meta, one = False):
+    if not one: 
+      self.state.reset()
+      self.contextstack.reset()
+
     steps = flow_meta['steps']
+    if not self.state.ready():
+      self.state.set(steps)
+
     image = None
 
     execute = True
-    while(execute and self.counter < len(steps)):
-      
-      current_step = steps[self.counter]
-      self.increment_counter()
-
-      image = self.run_step(current_step)
-
-      if one == True: 
+    while(execute):
+      current_step = self.state.next()
+      if current_step is not None:
+        image = self.run_step(current_step)
+        if one == True: 
+          execute = False
+      else:
         execute = False
 
+
     return image
-
-
 
 
   def run_step(self, current_step):
@@ -87,18 +85,22 @@ class Runner():
 
 
   def back(self):
-    if self.contextstack.isEmpty() or self.counter == 0:
+
+    image = None
+    if self.state.prev() == None:
+      self.state.reset()
+      self.contextstack.reset()
       print("On the top")
       return None
-
-    kwargs = self.contextstack.peek().kwargs_before
-    self.contextstack.pop()
-    self.dencrement_counter()
+    else:
+      image = self.contextstack.peek().kwargs_before['image']
+      self.contextstack.pop()
     
-    return kwargs['image']  
+    return image
 
 
   def top(self):
+    self.state.reset()
     self.contextstack.reset()
-    self.counter = 0
+
     return
