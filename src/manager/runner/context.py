bf14b374788a5bc39d607b23b785317c551872b5
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-04-28 10:49:00 +0300

    Simplify context

--- src/manager/runner/context.py
+++ src/manager/runner/context.py
@@ -1,6 +1,5 @@
 class Context:
-  def __init__(self, step_meta, **kwargs):
-    self.step_meta = step_meta
+  def __init__(self, **kwargs):
     self.kwargs_before = kwargs
     self.kwargs_after = None
 
