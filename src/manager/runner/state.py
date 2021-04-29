6e8f0a48fefc41694c4677c185bc87bff65e1a95
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-04-29 16:09:52 +0300

    Add statement into state

--- src/manager/runner/state.py
+++ src/manager/runner/state.py
@@ -1,4 +1,37 @@
 class State:
+  class Statement:
+    def __init__(self, parent):
+      self.parent = parent
+      self.type = None
+      self.param = {}
+      self.counter = 0
+      self.begin = 0
+      self.end = 0
+
+    def set(self, step):
+      self.counter = self.parent.counter
+      gforinrange = step.get('gforinrange', None)
+      if gforinrange is not None:
+        self.type = 'gforinrange'
+        self.param['x'] = step['gforinrange'].get('x', 0)
+        self.param['y'] = step['gforinrange'].get('y', 10)
+        self.param['s'] = step['gforinrange'].get('s', 1)
+        self.param['d'] = step['gforinrange'].get('d', 0)
+        self.param['i'] = step['gforinrange'].get('i', '')
+
+        self.begin = self.counter
+        self.end = self.begin + self.param['d']
+
+      return
+
+    def get_counter(self):
+      self.counter += 1
+      if self.type and (self.counter >= self.end):
+        self.counter = self.begin
+
+      return self.counter
+
+
   def __init__(self):
     self.reset()
 
@@ -6,6 +39,7 @@ class State:
   def reset(self):
     self.counter = -1
     self.steps = None
+    self.statement = self.Statement(self)
 
     return    
 
@@ -24,7 +58,8 @@ class State:
     step = None
     if self.counter < len(self.steps):
       step = self.steps[self.counter]
-      self.counter += 1
+      self.statement.set(step)      
+      self.counter = self.statement.get_counter()
 
     return step
 
