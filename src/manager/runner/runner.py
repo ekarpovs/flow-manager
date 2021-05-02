157c624720a154d7036fd50da4527c1b9d2c97af
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-05-02 17:09:59 +0300

    Rename counter

--- src/manager/runner/runner.py
+++ src/manager/runner/runner.py
@@ -10,14 +10,14 @@ class Runner():
     self.cv2image = None
     self.statement_stack = Stack()
     self.context_stack = Stack()
-    self.step_counter = 0
+    self.step_index = 0
 
 
 # INITIALIZATION 
   def reset(self):
     self.statement_stack.reset()
     self.context_stack.reset()
-    self.step_counter = 0
+    self.step_index = 0
 
     return
 
@@ -55,6 +55,20 @@ class Runner():
     self.context_stack.push(step_context)
 
     return kwargs['image']
+    
+
+  def calculate_next_step_index(self):
+    
+    self.step_index += 1
+
+    return
+
+
+  def calculate_prev_step_index(self):
+    
+    self.step_index -= 1
+
+    return
 
 
   def run(self, flow_meta, one = False):
@@ -66,29 +80,29 @@ class Runner():
 
     execute = True
     while(execute):
-      current_step = steps[self.step_counter]
-      if (current_step is not None) and (self.step_counter < len(steps)):
+      current_step = steps[self.step_index]
+      if (current_step is not None) and (self.step_index < len(steps)):
         image = self.run_step(current_step)
-        if self.step_counter < len(steps) - 1:
-          self.step_counter += 1
+        if self.step_index < len(steps) - 1:
+          self.calculate_next_step_index()
         if one == True: 
           execute = False
       else:
         execute = False
 
-    return self.step_counter, image
+    return self.step_index, image
 
 
 # PLAYBACK
   def back(self):
     image = None
 
-    if self.step_counter > 0:
+    if self.step_index > 0:
       image = self.context_stack.peek().kwargs_before['image']
       self.context_stack.pop()
-      self.step_counter -= 1
+      self.calculate_prev_step_index()
 
-    return self.step_counter, image
+    return self.step_index, image
 
 
   def top(self):
