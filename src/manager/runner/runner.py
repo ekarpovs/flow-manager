f3aacf8c98034dd33dfeaa0d40f35b756ee77528
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-05-02 16:52:45 +0300

    Change step counting

--- src/manager/runner/runner.py
+++ src/manager/runner/runner.py
@@ -1,55 +1,43 @@
 from tkinter.constants import E
-from .state import State
+from .statement import Statement 
 from .context import Context
 from .stack import Stack
 from .wrapper import flowoperation
 
 class Runner():
   def __init__(self, cfg):
-    self.state = State()
-    self.contextstack = Stack()
     self.get = cfg.get_factory().get
     self.cv2image = None
- 
+    self.statement_stack = Stack()
+    self.context_stack = Stack()
+    self.step_counter = 0
+
+
+# INITIALIZATION 
+  def reset(self):
+    self.statement_stack.reset()
+    self.context_stack.reset()
+    self.step_counter = 0
+
+    return
+
   def set_input_image(self, cv2image):
     self.cv2image = cv2image
 
     return
 
+
+# EXECUTION
   def init_step(self):
-    if self.contextstack.isEmpty():
+    if self.context_stack.isEmpty():
       # self.state.reset()
       kwargs = {}
       kwargs['orig'] = self.cv2image
       kwargs['image'] = self.cv2image
     else:
-      kwargs = self.contextstack.peek().kwargs_after
+      kwargs = self.context_stack.peek().kwargs_after
 
     return kwargs
-
-
-  def run(self, flow_meta, one = False):
-    if not one: 
-      self.state.reset()
-      self.contextstack.reset()
-
-    steps = flow_meta['steps']
-    if not self.state.ready():
-      self.state.set(steps)
-
-    image = None
-
-    execute = True
-    while(execute):
-      counter, current_step = self.state.next()
-      if current_step is not None:
-        image = self.run_step(current_step)
-        if one == True: 
-          execute = False
-      else:
-        execute = False
-
-    return counter, image
 
 
   def run_step(self, current_step):
@@ -64,30 +52,47 @@ class Runner():
     # Set result to step context
     step_context.set_after(**kwargs)
     # Store the context
-    self.contextstack.push(step_context)
+    self.context_stack.push(step_context)
 
     return kwargs['image']
 
 
-  def back(self):
+  def run(self, flow_meta, one = False):
+    if not one: 
+      self.reset()
 
     image = None
-    counter, current_step = self.state.prev()
-    if current_step is None:
-      self.state.reset()
-      self.contextstack.reset()
-      print("On the top")
-      counter = 0
-      return counter, None
-    else:
-      image = self.contextstack.peek().kwargs_before['image']
-      self.contextstack.pop()   
+    steps = flow_meta['steps']
 
-    return counter, image
+    execute = True
+    while(execute):
+      current_step = steps[self.step_counter]
+      if (current_step is not None) and (self.step_counter < len(steps)):
+        image = self.run_step(current_step)
+        if self.step_counter < len(steps) - 1:
+          self.step_counter += 1
+        if one == True: 
+          execute = False
+      else:
+        execute = False
+
+    return self.step_counter, image
+
+
+# PLAYBACK
+  def back(self):
+    image = None
+
+    if self.step_counter > 0:
+      image = self.context_stack.peek().kwargs_before['image']
+      self.context_stack.pop()
+      self.step_counter -= 1
+
+    return self.step_counter, image
 
 
   def top(self):
-    self.state.reset()
-    self.contextstack.reset()
+    print("top")
 
-    return
+    return self.reset()
+
