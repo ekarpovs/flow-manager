36787dc927460fc9ca68ecd54b77d0c4198abfa6
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-05-17 11:44:31 +0300

    Add button - clear output windows

--- src/mainactions.py
+++ src/mainactions.py
@@ -19,23 +19,34 @@ class MainActions(Frame):
     self.columnconfigure(3, weight=1)
     self.columnconfigure(4, weight=1)
 
-    stdout_textbox=ScrolledText(self, height=6, width=105,  wrap=WORD)
-    stdout_textbox.grid(row=0, column=0, columnspan=2, padx=PADX, pady=PADY, sticky=W+S)
+    self.stdout_textbox=ScrolledText(self, height=6, width=105,  wrap=WORD)
+    self.stdout_textbox.grid(row=0, column=0, columnspan=2, padx=PADX, pady=PADY, sticky=W+S)
 
-    stderr_textbox=ScrolledText(self, height=6, width=105,  wrap=WORD)
-    stderr_textbox.grid(row=0, column=2, columnspan=2, padx=PADX, pady=PADY, sticky=W+S)
+    self.stderr_textbox=ScrolledText(self, height=6, width=105,  wrap=WORD)
+    self.stderr_textbox.grid(row=0, column=2, columnspan=2, padx=PADX, pady=PADY, sticky=W+S)
 
-    self.btn_exit = Button(self, text='Exit', width=BTNW)
-    self.btn_exit.grid(row=0, column=4, padx=PADX, pady=PADY, sticky=E)
+    bfr = Frame(self)
+    bfr.grid(row=0, column=4, padx=PADX, pady=PADY, sticky=E)
 
-    def stdout_redirector(line):
-      stdout_textbox.insert(END, line)
-      stdout_textbox.yview(END)
+    self.btn_clear = Button(bfr, text='Clear', width=BTNW, command=self.clear)
+    self.btn_clear.grid(row=0, column=0, padx=PADX, pady=PADY, sticky=E)
 
-    def stderr_redirector(line):
-      stderr_textbox.insert(END, line)
-      stderr_textbox.yview(END)
+    self.btn_exit = Button(bfr, text='Exit', width=BTNW)
+    self.btn_exit.grid(row=1, column=0, padx=PADX, pady=PADY, sticky=E)
+
         
+    sys.stdout.write = self.stdout_redirector
+    sys.stderr.write = self.stderr_redirector
 
-    sys.stdout.write = stdout_redirector
-    sys.stderr.write = stderr_redirector
+
+  def clear(self):
+    self.stdout_textbox.delete(1.0, END)
+    self.stderr_textbox.delete(1.0, END)
+
+  def stdout_redirector(self, line):
+    self.stdout_textbox.insert(END, line)
+    self.stdout_textbox.yview(END)
+
+  def stderr_redirector(self, line):
+    self.stderr_textbox.insert(END, line)
+    self.stderr_textbox.yview(END)
