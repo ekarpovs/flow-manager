fb1d5dd96e43a7438d826a8fd0e9825cc108c943
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-05-17 11:12:00 +0300

    Redirect stdout/err from console to mainaction

--- src/mainactions.py
+++ src/mainactions.py
@@ -1,8 +1,9 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter.scrolledtext import *
 
+from tkinter import ttk
+import sys
 from .uiconst import *
-from .configdialog import ConfigDialog
 
 class MainActions(Frame):
   def __init__(self, parent):
@@ -12,17 +13,32 @@ class MainActions(Frame):
     self["bg"] = "green"
 
     self.grid()
-    self.rowconfigure(0, weight=1)
+    self.columnconfigure(0, weight=1)
+    self.columnconfigure(1, weight=1)
     self.columnconfigure(2, weight=1)
+    self.columnconfigure(3, weight=1)
+    self.columnconfigure(4, weight=1)
 
-    self.btn_config = Button(self, text='Config', width=BTNW, command=self.config)
-    self.btn_config.grid(row=0, column=0, padx=PADX, pady=PADY)
+    stdout_textbox=ScrolledText(self, height=6, width=105,  wrap=WORD)
+    stdout_textbox.grid(row=0, column=0, columnspan=2, padx=PADX, pady=PADY, sticky=W+S)
 
-    self.btn_doc = Button(self, text='Doc', width=BTNW)
-    self.btn_doc.grid(row=0, column=1, padx=PADX, pady=PADY)
+    stderr_textbox=ScrolledText(self, height=6, width=105,  wrap=WORD)
+    stderr_textbox.grid(row=0, column=2, columnspan=2, padx=PADX, pady=PADY, sticky=W+S)
 
     self.btn_exit = Button(self, text='Exit', width=BTNW)
-    self.btn_exit.grid(row=0, column=2, padx=PADX, pady=PADY, sticky=E)
+    self.btn_exit.grid(row=0, column=4, padx=PADX, pady=PADY, sticky=E)
+
+    def stdout_redirector(line):
+      stdout_textbox.insert(END, line)
+      stdout_textbox.yview(END)
+
+    def stderr_redirector(line):
+      stderr_textbox.insert(END, line)
+      stderr_textbox.yview(END)
+        
+
+    sys.stdout.write = stdout_redirector
+    sys.stderr.write = stderr_redirector
 
 
   def config(self):
