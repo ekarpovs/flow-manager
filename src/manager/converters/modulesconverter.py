593cdafa14bcf69af4e3dbd24b41d0ce8a3f14b9
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-07-11 11:26:27 +0300

    Clean code

--- src/manager/converters/modulesconverter.py
+++ src/manager/converters/modulesconverter.py
@@ -2,25 +2,6 @@ import re
 from .converter import Converter
 
 '''
-  {
-    'path': '../modules-and-worksheets/modules-common', 
-    'modules': [
-      {
-        'name': 'common', 
-        'meta': {
-          'descr': '\nLocal module, contains common utility operations\n', 
-          'func': [
-            'empty', 
-            'start', 
-            'store', 
-            'restore', 
-            'clean', 
-            'printkwargs'
-          ]
-        }
-      }
-    ]
-  }  
 '''
 
 class ModulesConverter(Converter):
@@ -102,24 +83,3 @@ class ModulesConverter(Converter):
       oper_params.append({"type": t, "domain": d, "p_values": pvs, "name": p_name, "value": p_value, "label": l})
 
     return oper_params
-
-
-  # @staticmethod
-  # def convert_params_defenition_to_default_params(params_defenition, orig_image_size = (0,0)):
-  #   oper_params = []
-  #   for param_def in params_defenition:
-  #     t, d, pvs, df, l = ModulesConverter.parse_single_param_defenition(param_def)
-  #     # print("t, d, pvs, df, l", t, d, pvs, df, l)
-  #     p_name = l.split(':')[0].strip()
-  #     (h, w) = orig_image_size
-  #     if (h > 0) and (w > 0):
-  #       if df == 'h':
-  #         df = h
-  #       if df == 'w':
-  #         df = w
-
-  #     p_value = df
-
-  #     oper_params.append({"type": t, "domain": d, "p_values": pvs, "name": p_name, "value": p_value, "label": l})
-
-  #   return oper_params
