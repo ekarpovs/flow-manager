e5fc3de2f9a5e8e262ea1c3a813ffd1dbe6c9449
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-08-22 18:11:38 +0300

    Simplify plot usage

--- src/manager/views/imagesview.py
+++ src/manager/views/imagesview.py
@@ -1,11 +1,14 @@
 from tkinter import *
-from PIL import Image, ImageTk
+# from PIL import Image, ImageTk
 from tkinter import ttk
-import cv2
+# import cv2
 import matplotlib
-matplotlib.use("TkAgg")
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-from matplotlib.figure import Figure
+matplotlib.use("agg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+# from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+# import matplotlib.animation as animation
+
 from ...uiconst import *
 from .view import View
 
@@ -43,7 +46,7 @@ class ImagesView(View):
     self.names_combo_box.grid(row=2, column=0, padx=PADX, pady=PADY, sticky=N+S+W+E)
     self.btn_load.grid(row=2, column=1, padx=PADX, pady=PADY, sticky=W + S)
     self.file_names_list_box.grid(row=3, column=0, columnspan=2, padx=PADX, pady=PADY, sticky=N+S+W+E)
-
+    
 
   def set_input_paths(self, paths):
     self.names_combo_box['values'] = paths
@@ -71,49 +74,17 @@ class ImagesView(View):
     self.file_names_list_box.event_generate("<<ListboxSelect>>")
     return
 
-  def calculate_new_size(self, image):
-    (h, w) = image.shape[:2]
-    ratio = w/h
-    if h > self.panel_height:
-      h = self.panel_height
-      w = int(h*ratio)
-    elif w > self.panel_width:    
-      w = self.panel_width
-      h = int(w/ratio)   
-    self.h = h
-    self.w = w
-    return
-
   def set_result_image(self, cv2image):
-    figure = Figure(figsize=(5,5), dpi=100)
-    # Adds a subplot at the 1st position
-    splot = figure.add_subplot(111)
+    # !!! Memory leak
+    figure = plt.figure(1); 
+    plt.clf()
+    plt.axis('off')
+
     canvas = FigureCanvasTkAgg(figure, self)
     canvas.draw()
     wdg = canvas.get_tk_widget()
     wdg.grid(row=0, column=0, padx=PADX, pady=PADY, columnspan=2, sticky=W + E + N + S)
-
-    toolbar = NavigationToolbar2Tk(canvas, wdg)
-    toolbar.update()
-    splot.imshow(cv2image,  cmap='gray')
+   
+    plt.imshow(cv2image, cmap='gray')
     return
 
-  # def fit_image_to_panel(self, image):
-  #   self.calculate_new_size(image)   
-  #   dim = (self.w, self.h)
-  #   image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
-  #   return image
-
-  # def set_result_image_orig(self, cv2image):
-  #   cv2image = self.fit_image_to_panel(cv2image)
-  #   image = Image.fromarray(cv2image)
-  #   imagetk = ImageTk.PhotoImage(image=image)
-  #   # Show the image
-  #   self.set_content(self.image_label, 0, 0, imagetk)
-  #   return
-
-  # def set_content(self, label, r, c, imagetk):
-  #   label = Label(self, text='Image', image=imagetk, borderwidth=2, relief="solid")
-  #   label.image = imagetk
-  #   label.grid(row=r, column=c, padx=PADX, pady=PADY, columnspan=2, sticky=W + E + N + S)
-  #   return
