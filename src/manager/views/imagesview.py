bc7675a4f7665e429ea42e16405c2752c84db763
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-10-06 14:50:48 +0300

    Add axis and navbar to plotter

--- src/manager/views/imagesview.py
+++ src/manager/views/imagesview.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 # import cv2
 import matplotlib
 matplotlib.use("agg")
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 # from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 # import matplotlib.animation as animation
@@ -17,20 +17,16 @@ class ImagesView(View):
     super().__init__(parent)
     self.parent = parent 
 
-    # self['bg'] = "snow2"
-    self['text'] = 'Input/Output'
-
     self.panel_height, self.panel_width = get_panel_size(parent)
     
     self.grid()
     self.rowconfigure(0, weight=1)
-    self.rowconfigure(1, minsize=40)
-    self.rowconfigure(2, minsize=40)
+    self.rowconfigure(1, minsize=20)
+    self.rowconfigure(2, minsize=20)
+    self.rowconfigure(3, minsize=40)
     self.columnconfigure(0, weight=1)
 
-    # Init the original image holder
-    self.image_label = Label(self, text='Image', image=None, borderwidth=2, relief="solid")
-    self.image_label.image = None
+    self.figure = plt.figure(1); 
 
     self.namesvar = StringVar()
     self.names_combo_box = ttk.Combobox(self, textvariable=self.namesvar, font=("TkDefaultFont"))
@@ -43,11 +39,10 @@ class ImagesView(View):
     self.modulesvar = StringVar(value=self.paths)
     self.file_names_list_box = Listbox(self, height=10, listvariable=self.modulesvar, selectmode=BROWSE)
 
-    self.image_label.grid(row=0, column=0, columnspan=3, padx=PADX, pady=PADY, sticky=W + E + N + S)
-    self.names_combo_box.grid(row=1, column=0, padx=PADX, pady=PADY, sticky=N+S+W+E)
-    self.btn_load.grid(row=1, column=1, padx=PADX, pady=PADY, sticky=W + S)
-    self.btn_clear.grid(row=1, column=2, padx=PADX, pady=PADY, sticky=W + S)
-    self.file_names_list_box.grid(row=2, column=0, columnspan=3, padx=PADX, pady=PADY, sticky=N+S+W+E)
+    self.names_combo_box.grid(row=2, column=0, padx=PADX, pady=PADY, sticky=N+S+W+E)
+    self.btn_load.grid(row=2, column=1, padx=PADX, pady=PADY, sticky=W + S)
+    self.btn_clear.grid(row=2, column=2, padx=PADX, pady=PADY, sticky=W + S)
+    self.file_names_list_box.grid(row=3, column=0, columnspan=3, padx=PADX, pady=PADY, sticky=N+S+W+E)
     
 
   def set_input_paths(self, paths):
@@ -78,30 +73,16 @@ class ImagesView(View):
 
   def set_result_image(self, cv2image):
     # !!! Memory leak
-    figure = plt.figure(1); 
     plt.clf()
-    plt.axis('off')
 
-    canvas = FigureCanvasTkAgg(figure, self)
+    canvas = FigureCanvasTkAgg(self.figure, self)
     canvas.draw()
-    wdg = canvas.get_tk_widget()
-    wdg.grid(row=0, column=0, padx=PADX, pady=PADY, columnspan=3, sticky=W + E + N + S)
-   
+    canvas.get_tk_widget().grid(row=0, column=0, padx=PADX, pady=PADY, columnspan=3, sticky=W + E + N + S)
+   # navigation toolbar
+    tb_frame = Frame(self)
+    tb_frame.grid(row=1, column=0, padx=PADX, pady=PADY, columnspan=3, sticky=W + E + N + S)
+    NavigationToolbar2Tk(canvas, tb_frame)
+
     plt.imshow(cv2image, cmap='gray')
     return
 
-  # def auto_input(self):
-  #   if self.autoinputvar.get():
-  #     self.auto_input_check_button.event_generate("<<CheckbuttonChecked>>")
-  #   else:
-  #     self.auto_input_check_button.event_generate("<<CheckbuttonUnChecked>>")
-  #   return
-
-  # def activate_controls(self, activate=False):
-  #   state = DISABLED
-  #   if activate:
-  #     state = NORMAL
-  #   self.names_combo_box['state']=state
-  #   self.file_names_list_box['state']=state
-  #   self.btn_load['state']=state
-  #   return
