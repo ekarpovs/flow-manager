cbf513b3dc4c6a8568c397a5ebf024a825a160dc
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-12-19 16:42:03 +0200

    Change type const

--- src/manager/mngrcontroller.py
+++ src/manager/mngrcontroller.py
@@ -189,7 +189,7 @@ class MngrController():
       for ref in out_refs:
         (ref_name, ref_type) = ref
         data = out_data[ref_name]
-        if ref_type == FlowDataType.IMAGE:
+        if ref_type == FlowDataType.CV2_IMAGE:
           if data is not None:
             image = cv2.cvtColor(data, cv2.COLOR_BGR2RGB)
             self._view.data.set_result_image(image)
