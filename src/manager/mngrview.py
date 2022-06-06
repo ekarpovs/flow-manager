788cd26754e363c62990482863c66b1619b6d6a3
Evgeny Karpovsky <ekarpovsky@gmail.com>
2022-06-06 11:04:41 +0300

    Change view widgets hierarchy

--- src/manager/mngrview.py
+++ src/manager/mngrview.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 from ..uiconst import *
 from .views import *
 
-class MngrView(LabelFrame):
+class MngrView(Frame):
   '''
   Fasade for views 
   '''
@@ -21,18 +21,17 @@ class MngrView(LabelFrame):
     self.columnconfigure(3, weight=2)
 
 
-    self._module = ModuleView(self)
-    self._flow = FlowView(self)
-    self._links = LinksView(self)
-    self._params = ParamsView(self)
-    self._data = DataView(self)
+    self._module = ModuleView(self.parent)
+    self._flow = FlowView(self.parent)
+    self._links = LinksView(self.parent)
+    self._params = ParamsView(self.parent)
+    self._data = DataView(self.parent)
 
     self._module.grid(row=0, column=0, rowspan=2, sticky=N+S)
     self._flow.grid(row=0, column=1, sticky=N+S)
     self._links.grid(row=1, column=1, sticky=N+S)
     self._params.grid(row=0, column=2, rowspan=2, sticky=N+S)
     self._data.grid(row=0, column=3, rowspan=2, sticky=N+S)
-    self._divide_view()
     return
 
   @property
@@ -84,30 +83,4 @@ class MngrView(LabelFrame):
   @module_defs.setter
   def module_defs(self, modules_defs):
     self._module.module_defs = modules_defs
-    return
-
-# View layout
-################### Local methods ###########################
-  def _divide_view(self):
-    # self.parent.update()
-    self.update_idletasks()
-    h = self.parent.winfo_reqheight()
-    w = self.parent.winfo_reqwidth()
-    self['width'] = w - PADX_S
-    view_height = h - PADY*2
-    self._module['height'] = view_height
-    self._module['width'] = w//5
-    self._flow['height'] = int(view_height * 0.7)
-    self._flow['width'] = int((w/4)*0.95)
-    self._links['height'] = int(view_height * 0.3)
-    self._links['width'] = int((w/4)*0.95)
-    self._params['height'] = view_height
-    self._params['width'] = int((w/5) * 1.1)
-    self._data['height'] = view_height
-    self._data['width'] = w - self._module['width'] - self._params['width'] - self._flow['width']-PADX
-    self._module.grid_propagate(0)
-    self._flow.grid_propagate(0)
-    self._links.grid_propagate(0)
-    self._params.grid_propagate(0)
-    self._data.grid_propagate(0)
     return
