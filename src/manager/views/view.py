282c6a2cef8e015e94246801e95d20c9757e1e73
Evgeny Karpovsky <ekarpovsky@gmail.com>
2022-06-06 11:06:49 +0300

    Store view geometry for use in derived   views

--- src/manager/views/view.py
+++ src/manager/views/view.py
@@ -5,7 +5,11 @@ from ...uiconst import *
 class View(LabelFrame):
   def __init__(self, parent):
     super().__init__(parent)
-    self.parent = parent 
+    self._parent = parent 
+
+    self._parent.update_idletasks()
+    self._manager_container_h = self._parent.winfo_height()
+    self._manager_container_w = self._parent.winfo_width()
 
     self['bd'] = 2
     self['relief'] = RIDGE
